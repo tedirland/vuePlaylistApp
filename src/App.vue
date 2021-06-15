e99db74c334414524bdab617af8feace7960d63c
Ted Irland <irlandth@gmail.com>
2021-06-15 09:47:29 -0500

    Created navbar component to be used across all views

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
+<Navbar />
 <div class="content">
   <router-view/>
   </div>
 
 </template>
+
+<script>
+import  Navbar  from './components/Navbar.vue'
+
+export default {
+  components: {Navbar},
+  setup() {
+
+  },
+}
+</script>
 
 <style>
 .content {
