5b663b7a4da3e1872c8ddcdf9b2f115f736f5b87
Ted Irland <irlandth@gmail.com>
2021-06-15 12:51:01 -0500

    Created a new playlist component and styled it. Created a route to display when the browser is on /playlists/create

---
+++ src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h4>Create New Playlist</h4>
+    <input type="text" required placeholder="playlist title" v-model="title">
+    <textarea required placeholder="playlist description..." v-model="description"></textarea>
+    <!-- upload playlist image -->
+    <label>Upload Playlist Cover Image</label>
+    <input type="file">
+
+    <div v-if="error" class="error">{{error}}</div>
+
+    <button>Create</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+setup() {
+  const title = ref('')
+  const description = ref('')
+  const error = ref(null)
+
+  const handleSubmit = () => {
+console.log(title.value, description.value)
+  }
+  return {title, description, handleSubmit, error}
+}
+}
+</script>
+
+<style>
+ form {
+    background: white;
+  }
+  input[type="file"] {
+    border: 0;
+    padding: 0;
+  }
+  label {
+    font-size: 12px;
+    display: block;
+    margin-top: 30px;
+  }
+  button {
+    margin-top: 20px;
+  }
+</style>
