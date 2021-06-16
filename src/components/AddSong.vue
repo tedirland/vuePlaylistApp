1eedc624eed4a59a3f8f98924e01a13f02cacbfc
Ted Irland <irlandth@gmail.com>
2021-06-16 13:56:32 -0500

    Created a form to collect song info, a button that displays the form when needed and added all the form values as refs

---
+++ src/components/AddSong.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="add-song">
+    <button v-if="!showForm" @click="showForm = true">Add To Playlist</button>
+    <form v-if="showForm" @submit.prevent="handleSubmit">
+      <h4>Add a new Song</h4>
+      <input type="number" min="1" max="10" placeholder="Ranking" required v-model="ranking">
+      <input type="text" placeholder="Song Title" required v-model="title">
+      <input type="text" placeholder="Artist" required v-model="artist">
+      <input type="text" placeholder="Youtube Url" required v-model="youtubeUrl">
+      <input type="text" placeholder="Description" required v-model="description">
+      <button>Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+setup() {
+  const title = ref('')
+  const artist = ref('')
+  const ranking = ref('')
+  const youtubeUrl = ref('')
+  const description = ref('')
+  const showForm = ref(false)
+
+const handleSubmit = async () => {
+  const newSong = {
+    title: title.value,
+    artist: artist.value,
+    ranking: ranking.value,
+    id: Math.random(Math.random() *1000000),
+    youtubeUrl: youtubeUrl.value,
+    description: description.value,
+  }
+  console.log(newSong)
+}
+
+return {title, artist, ranking, youtubeUrl, description, showForm, handleSubmit}
+}
+}
+</script>
+
+<style scoped>
+ .add-song {
+    text-align: center;
+    margin-top: 40px;
+  }
+  form {
+    max-width: 100%;
+    text-align: left;
+  }
+</style>
