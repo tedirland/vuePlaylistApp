4d03a129ea091a418e0619ca39f66ac8e2237263
Ted Irland <irlandth@gmail.com>
2021-06-16 14:33:44 -0500

    Created delete functionality for songs on a playlist

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,67 +1,74 @@
 <template>
+  <div class="error" v-if="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
 
-<div v-if="error" class="error">{{error}}</div>
-
-<div v-if="playlist" class="playlist-details">
-  <!-- Playlist Information -->
-  <div class="playlist-info">
-    <div class="cover">
-      <img :src="playlist.coverUrl">
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl">
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+      <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
     </div>
-    <h2>{{playlist.title}}</h2>
-    <p class="username">Created by {{playlist.userName}}</p>
-    <p class="description">{{playlist.description}}</p>
-    <button @click="handleDelete" v-if="ownership">Delete Playlist</button>
+
+    <!-- song list -->
+    <div class="song-list">
+      <div v-if="!playlist.songs.length">No songs have been added to this playlist yet.</div>
+      <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+        <div class="details">
+          <h3>{{ song.title }}</h3>
+          <p>{{ song.artist }}</p>
+        </div>
+        <button v-if="ownership" @click="handleClick(song.id)">delete</button>
+      </div>
+      <AddSong :playlist="playlist" />
+    </div>
+
   </div>
-  <!-- Song list -->
-  <div class="song-list">
-    <p>Song List here</p>
-    <AddSong v-if="ownership" :playlist="playlist" />
-  </div>
-</div>
 </template>
 
 <script>
 import AddSong from '@/components/AddSong.vue'
+import useStorage from '@/composables/useStorage'
+import useDocument from '@/composables/useDocument'
 import getDocument from '@/composables/getDocument'
 import getUser from '@/composables/getUser'
-import useDocument from '@/composables/useDocument'
-import useStorage from '@/composables/useStorage'
-import { computed } from '@vue/runtime-core'
+import { computed } from 'vue'
 import { useRouter } from 'vue-router'
-
-
-
-
-
 export default {
   props: ['id'],
-  components: {AddSong},
+  components: { AddSong },
   setup(props) {
-    const {error, document: playlist} = getDocument('playlists', props.id )
-    const {user} = getUser()
-    const { deleteDoc } = useDocument('playlists', props.id)
-    const {deleteImage} = useStorage()
+    const { error, document: playlist } = getDocument('playlists', props.id)
+    const { user } = getUser()
+    const { deleteDoc, updateDoc } = useDocument('playlists', props.id)
+    const { deleteImage } = useStorage()
     const router = useRouter()
-
     const ownership = computed(() => {
-      return playlist.value && user.value && user.value.uid == playlist.value.userId
+      return playlist.value
+        && user.value
+        && user.value.uid == playlist.value.userId
     })
-
     const handleDelete = async () => {
-      await deleteImage(playlist.value.filePath)
       await deleteDoc()
-     router.push({name: 'Home'})
+      await deleteImage(playlist.value.filePath)
+      router.push({ name: 'Home' })
     }
 
-    return {error, playlist, ownership, handleDelete}
-  }
+    const handleClick = async (id) => {
+      const songs = playlist.value.songs.filter((song) => song.id != id)
+      await updateDoc({songs })
 
+    }
+    return { error, playlist, ownership, handleDelete, handleClick }
+  }
 }
 </script>
 
 <style>
-.playlist-details {
+  .playlist-details {
     display: grid;
     grid-template-columns: 1fr 2fr;
     gap: 80px;
@@ -99,4 +106,13 @@ export default {
   .description {
     text-align: left;
   }
+  .single-song {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px dashed var(--secondary);
+    margin-bottom: 20px;
+  }
 </style>
+© 2021 GitHub, Inc.
